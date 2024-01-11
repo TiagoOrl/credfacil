1845d39cee38d0edce732839be790f9bb82d1e87
TiagoOrl <tiago_orlando@yahoo.com>
2024-01-10 21:48:43 -0300

    fix: recreate database migration files

--- src/main/resources/db/migration/V2__create_table_credit.sql
+++ src/main/resources/db/migration/V2__create_table_credit.sql
@@ -3,10 +3,12 @@ CREATE TABLE credit (
    credit_code UUID NOT NULL,
    credit_value DECIMAL NOT NULL,
    day_first_installment date NOT NULL,
-   num_of_installments INT NOT NULL,
+   number_of_installments INT NOT NULL,
    status SMALLINT,
-   fk_client_id BIGINT,
+   customer_id BIGINT,
    CONSTRAINT pk_credit PRIMARY KEY (id)
 );
 
-ALTER TABLE credit ADD CONSTRAINT FK_CREDIT_ON_FK_CLIENT FOREIGN KEY (fk_client_id) REFERENCES client (id);
+ALTER TABLE credit ADD CONSTRAINT uc_credit_credit_code UNIQUE (credit_code);
+
+ALTER TABLE credit ADD CONSTRAINT FK_CREDIT_ON_CUSTOMER FOREIGN KEY (customer_id) REFERENCES customer (id);
